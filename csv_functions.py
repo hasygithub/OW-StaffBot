a0bbdcab8bc37c74a38a31828883d68fd39fe29d
hasygithub <hasy.habib@gmail.com>
2019-05-31 22:25:09 -0400

    push

---
+++ csv_functions.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+def add_row_to_projects(partner_name,
+                        practice,
+                        level_required,
+                        office_preference,
+                        type_of_work_1,
+                        type_of_work_2,
+                        start_date,
+                        duration,
+                        attachment):
+
+    df = pd.read_csv('projects.csv')
+
+    row = [partner_name,
+           practice,
+           level_required,
+           office_preference,
+           type_of_work_1,
+           type_of_work_2,
+           start_date,
+           duration,
+           attachment]
+
+    df_row = pd.DataFrame(data=[row], columns=df.columns)
+    df_complete = df.append(df_row)
+    df_complete.to_csv('projects.csv')
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import pandas as pd
 
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -10,28 +10,48 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
-
-@app.route('/beach_requests/')
-def beach_requests():
-    df = pd.read_csv('beach_requests.csv')
-    return render_template('hello.html',
-                           num=len(list(df['request_id'])),
-                           request_id=list(df['request_id']),
+@app.route('/projects/')
+def projects():
+    df = pd.read_csv('projects.csv')
+    return render_template('projects.html',
+                           num=len(list(df['partner_name'])),
                            partner_name=list(df['partner_name']),
-                           request_description=list(df['request_description']),
-                           days_required=list(df['days_required']))
+                           practice=list(df['practice']),
+                           level_required=list(df['level_required']),
+                           office_preference=list(df['office_preference']),
+                           type_of_work_1=list(df['type_of_work_1']),
+                           type_of_work_2=list(df['type_of_work_2']),
+                           start_date=list(df['start_date']),
+                           duration=list(df['duration']),
+                           attachment=list(df['attachment'])
+                           )
 
+@app.route('/resources/')
+def resources():
+    df = pd.read_csv('resources.csv')
+    return render_template('resources.html',
+                           num=len(list(df['consultant_name'])),
+                           consultant_name=list(df['consultant_name']),
+                           practice=list(df['practice']),
+                           level=list(df['level']),
+                           office=list(df['office']),
+                           interest_1=list(df['interest_1']),
+                           interest_2=list(df['interest_2']),
+                           start_date=list(df['start_date']),
+                           one_pager=list(df['one_pager'])
+                           )
 
+@app.route('/input_test')
+def input_test():
+    return render_template('input_test.html')
 
-@app.route('/beach_resources/')
-def beach_requests():
-    df = pd.read_csv('beach_resources.csv')
-    return render_template('hello.html',
-                           num=len(list(df['request_id'])),
-                           request_id=list(df['request_id']),
-                           partner_name=list(df['partner_name']),
-                           request_description=list(df['request_description']),
-                           days_required=list(df['days_required']))
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    projectpath = request.form['projectFilepath']
+    return projectpath
+    # your code
+    # return a response
+
 
 
 @app.route('/hello/')
